7560238d4382ef8cb626bc05544d97edfbb59a16
Talkanbaev Artur <talkanbaev_a@auca.kg>
2023-06-27 19:54:23 +0600

    Added tempalte for display next func

--- README.md
+++ README.md
@@ -16,3 +16,9 @@ Ready to run in production? Please [check our deployment guides](https://hexdocs
   * Docs: https://hexdocs.pm/phoenix
   * Forum: https://elixirforum.com/c/phoenix-forum
   * Source: https://github.com/phoenixframework/phoenix
+
+## Link properties
+
+* og:title
+* og:video:url
+* og:description
--- lib/link_lang_classifier_web/live/classifier_live/index.ex
+++ lib/link_lang_classifier_web/live/classifier_live/index.ex
@@ -7,4 +7,11 @@ defmodule LinkLangClassifierWeb.ClassifierLive.Index do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  def get_next_link() do
+    # 1. Get next link from DB
+    # 2. Fetch the html by url with Finch
+    # 3. Parse HTML and get required tags
+    # 4. Put the required fields to a map and return
+  end
 end
