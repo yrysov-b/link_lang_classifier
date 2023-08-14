20cfd3ab7184601bc725eabf0c378d8a8d883fc2
Nel Escher <kescher@umich.edu>
2023-08-13 21:08:21 -0400

    fix deselect button on submit bug

--- lib/link_lang_classifier_web/live/classifier_live/index.ex
+++ lib/link_lang_classifier_web/live/classifier_live/index.ex
@@ -103,7 +103,7 @@ defmodule LinkLangClassifierWeb.ClassifierLive.Index do
         result = get_next_link(user_id)
         socket = put_flash(socket, :info, "Classified successfully.")
         new_count = count(user_id)
-        {:noreply, assign(socket, link: result, langs: @default_map, count: new_count)}
+        {:noreply, assign(socket, link: result, langs: @default_map, count: new_count, other_isChecked: false, none_isChecked: false)}
     end
   end
 
